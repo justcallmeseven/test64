30926ae870435adcc5083d7ddd146a62cf98f3bf
justcallmeseven <571621695@qq.com>
2018-06-20 10:56:53 +0800

    second commit

--- test64/src/test64/test64.java
+++ test64/src/test64/test64.java
@@ -5,6 +5,7 @@ public class test64 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hei Git!!");
+		System.out.println("Hei Git!!");
 	}
 
 }
